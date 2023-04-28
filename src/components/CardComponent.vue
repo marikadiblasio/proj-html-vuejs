56e06f62c24cbc3530b4c04e4dece5e954e0427d
marikadiblasio <marbla.albram@gmail.com>
2023-04-28 09:59:01 +0200

    add hover to cardcomponent

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,20 +1,28 @@
 <template>
     <div class="col-4 px-3 pb-5 d-flex">
-        <div :style="{backgroundImage: 'url' + '(' + show.image + ')'}" :class="{'active': index === store.activeIndex || !rounded, 'rounded-0': !rounded, 'rounded-4': rounded} " class="my-card pb-5">
-            <div class="text-end pt-3 pe-2">
-                <i class="fa-solid fa-star"></i>
-                <span>{{ show.voto }}/10</span>
+        <div :style="{backgroundImage: 'url' + '(' + show.image + ')'}" :class="{'active': index === store.activeIndex || !rounded, 'rounded-0': !rounded, 'rounded-4': rounded} " class="my-card p-relative pb-5">
+            <div class="perhov" :class="{'active': index === store.activeIndex || !rounded, 'rounded-0': !rounded, 'rounded-4': rounded} ">
+            <div class="d-flex justify-content-between pt-3 pe-2">
+                <div class="hours">
+                    <span>{{ show.duration }}</span>
+                </div>
+                <div>
+                    <i class="fa-solid fa-star"></i>
+                    <span>{{ show.voto }}/10</span>
+                </div>
             </div>
             <div class="card-bottom">
                 <h4 class="ps-2">{{show.title}}</h4>
                 <span class="ps-2">Category: {{ show.category }}</span>
-                <ButtonsComponent :views="'Views ' + show.views" :shared="false"/>
+                <ButtonsComponent :views="'Views ' + show.views" :shared="false" />
+            </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+
     import { store } from '../data/store';
     import ButtonsComponent from './ButtonsComponent.vue';
     export default {
@@ -49,10 +57,22 @@
         i{
             color: $my-contrast;
         }
+        .hours{
+            @include radius-right;
+            background-color: $my-contrast;
+            padding: 3px 10px;
+            opacity: 0;
+            }
         &.active{
             transform: scale(1.1);
-            &:hover{
-                filter: brightness(10%); //da modificare!!!
+            .perhov{
+                @include absolute-100;
+                &:hover{
+                    background: $my-bg-linear;
+                }
+            }
+            &:hover .hours{
+            opacity: 1;
             }
         }
     }
--- src/components/LatestNews.vue
+++ src/components/LatestNews.vue
@@ -61,9 +61,6 @@
             width: 40vw;
             height: 40vw;
         }
-        // h4, h5{
-        //     padding-top: 45%;
-        // }
         .big-square{
             background-image: url('/imgs/assets/blog-300x300.jpg');
             background-size: cover;
@@ -72,13 +69,7 @@
             position: relative;
 
             .perhov{
-            position: absolute;
-            top: 0;
-            left: 0;
-            bottom: 0;
-            right: 0;
-            width: 100%;
-            height: 100%;
+            @include absolute-100;
             background-color: rgba(0,0,0,0.3);
             }
         }
@@ -89,24 +80,13 @@
             width: 48%;
             height: 48%;
             position: relative;
-            // z-index: -2;
         }
-        // .smaller-square * {
-        //     z-index: 3;
-        // }
         .perhov{
-            position: absolute;
-            top: 0;
-            left: 0;
-            bottom: 0;
-            right: 0;
-            width: 100%;
-            height: 100%;
+            @include absolute-100;
             background-color: rgba(0,0,0,0.5);
         }
-
         .perhov:hover{
-            background: linear-gradient(180deg, rgba(19, 190, 19, 0.29) 1.82%, rgba(88, 199, 88, 0.445859) 52.25%, rgba(217, 217, 217, 0.06) 94.57%), linear-gradient(180deg, rgba(19, 190, 19, 0.65) 0%, rgba(217, 217, 217, 0.06) 52.08%);
+            background: $my-bg-linear;
         }
         .date{
             @include radius-left;
@@ -129,4 +109,5 @@
             right: 0;
         }
     }
+
 </style>
